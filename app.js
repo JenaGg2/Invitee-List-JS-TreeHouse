e5d2f1a26a9fd91b578164271f68b059bc2aab6d
Jeena.G <jeenagrg24@gmail.com>
2021-03-22 20:08:10 +0000

    Form input

---
+++ app.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('registrar');
+const input = form.querySelector('input');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+   console.log(input.value);
+})
